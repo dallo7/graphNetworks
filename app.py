a7a943d48be3108bf1a65cd16fd6bc8ff2632754
Dalmas Chituyi <122692086+dallo7@users.noreply.github.com>
2023-10-09 14:12:07 +0300

    Update app.py
    main

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('us.html')
+
+
+
+# Run this file as the main file...
+if __name__ == "__main__":
+    app.run(debug=True)
